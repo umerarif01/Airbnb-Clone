0f27ca4609e02abf5fbb141691c745c5022daa54
unknown <umerarif01@gmail.com>
2021-08-05 09:01:54 +0500

    front end

---
+++ components/Footer.js
@@ -0,0 +1,34 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32
+        py-14 bg-gray-100 text-gray-600 ">
+        <div className="space-y-4 text-xs text-gray-800">
+           <h5 className="font-bold">About</h5>
+           <p>How AirBnb Works</p>
+           <p>This is not a real site</p>
+           <p>Its a pretty awesome clone</p>
+           <p>Made by Umer A</p>
+           <p>Cool</p>
+           </div>
+           <div className="space-y-4 text-xs text-gray-800">
+           <h5 className="font-bold">Community</h5>
+           <p>Accessibilty</p>
+           <p>This is not a real site</p>
+           <p>Its a pretty awesome clone</p>
+           <p>Made by Umer A</p>
+           <p>Cool</p>
+           </div>
+           <div className="space-y-4 text-xs text-gray-800">
+           <h5 className="font-bold">Host</h5>
+           <p>Rent</p>
+           <p>This is not a real site</p>
+           <p>Its a pretty awesome clone</p>
+           <p>Made by Umer A</p>
+           <p>Cool</p>
+           </div>
+         
+        </div>
+    )
+}
+
+export default Footer
