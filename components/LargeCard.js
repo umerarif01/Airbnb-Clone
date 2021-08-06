ac3eff9e77eae87154c0d5ebabbf49b1b2fc951b
unknown <umerarif01@gmail.com>
2021-08-06 18:47:57 +0500

    day 2 build

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -2,8 +2,9 @@ import Image from 'next/image'
 
 function LargeCard({img,title,description,buttonText}) {
     return (
-        <section className="relative py-16 cursor-pointer">
-          <div className="relative h-96 min-w-[300px]">
+        <section className="relative py-16 cursor-pointer hover:scale-105 transform transition
+        duration-300 ease-out">
+          <div className="relative h-96 min-w-[300px] ">
           <Image src={img} layout="fill"
           objectFit="cover"
           className="rounded-2xl"/>  
